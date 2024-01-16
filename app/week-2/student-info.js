e655dd631b528a8f82308e61e239e1ca40253ecc
suetsuey <140632841+suetsuey@users.noreply.github.com>
2024-01-16 15:43:51 -0700

    assignment 2 done

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <main>
+      <p>Suet Ying Kong</p>
+      <Link href="https://github.com/suetsuey/week-2">https://github.com</Link>
+    </main>
+  );
+}
