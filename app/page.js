b3701997cdc57a17994caf1c06069440a7bb288c
suetsuey <140632841+suetsuey@users.noreply.github.com>
2024-02-06 16:31:33 -0700

    week 4 done

--- app/page.js
+++ app/page.js
@@ -11,6 +11,9 @@ export default function Page() {
         <li>
           <Link href="week-3">Week 3 Assignment</Link>
         </li>
+        <li>
+          <Link href="week-4">Week 4 Assignment</Link>
+        </li>
       </ul>
     </main>
   );
---
+++ app/week-4/page.js
@@ -0,0 +1,11 @@
+import NewItem from "./new-item";
+
+export default function Page() {
+  return (
+    <main>
+      <div className="bg-blue-10-1000">
+        <NewItem />
+      </div>
+    </main>
+  );
+}
